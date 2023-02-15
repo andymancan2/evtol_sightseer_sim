ba8a065f396d48630a6eab608f25a0fd2fa8956f
andymancan2 <60756542+andymancan2@users.noreply.github.com>
2023-02-15 18:31:12 -0500

    Breakout unit_test single function to many functions for better readability and understanding.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@
 
 using namespace std;
 
-/// \brief Bottom up unit test.
-/// TODO: Install google test then utilize mock classes for a top-down testing.
-void unitTest( void )
+/// \brief Unit tests for evtol_prop class.
+void evtol_prop_unitTest( )
 {
     try {
         evtol_prop tst1( "tst1", 1, 2, 3, 1, 0, 7);
@@ -44,11 +43,21 @@ void unitTest( void )
     tst.disp();
     evtol_prop tstA("alpha", 120, 320, .6, 1.6, 4, 0.25);
     tstA.disp();
+}
+
+/// \brief Unit tests for evtol_list class.
+void evtol_list_unitTest()
+{
     extern evtol_list evtolLst;
     evtolLst.disp();
     cout << "Perform visual inspection that above print statements match the company specs.\n";
     cout << "Alpha name = " << evtolLst.getCompanyProperty(C_ALPHA)->kName << endl;
     evtolLst.unitTest();
+}
+
+/// \brief Unit test for charge_stations class.
+void charge_stations_unitTest()
+{
     charger_stations cs;
     assert( cs.addVehToCharger(0) == true );
     assert( cs.addVehToCharger( 1 ) == true );
@@ -58,8 +67,15 @@ void unitTest( void )
     assert( cs.freeVehFromCharger( 2 ) == true );
     assert( cs.freeVehFromCharger( 1 ) == true );
     assert( cs.freeVehFromCharger( 0 ) == true );
+}
+
+/// \brief A unit test for the vechicle class for each company type.
+void vehicle_unitTest()
+{
+    extern evtol_list evtolLst;
+
     for (uint16_t company=C_ALPHA; company < C_NUM_COMPANIES; company++) {
-        vehicle v( (evtol_companies_e)company);
+        vehicle v( (evtol_companies_e)company );
         cout << "kCompany: " << v.kCompany << endl;
         assert(v.getCurrentState() == VS_IDLE);
         assert(v.sim() == NO_CHANGE);
@@ -68,7 +84,7 @@ void unitTest( void )
         //uint16_t vFlightMins = fabs(evtolLst.getCompanyProperty((evtol_companies_e)company)->kBattKwh /
         //                            evtolLst.getCruiseMinEnergy((evtol_companies_e)company));
         uint16_t vFlightMins = ceil((evtolLst.getCompanyProperty((evtol_companies_e)company)->kBattKwh
-                                       - evtolLst.getCruiseMinEnergy((evtol_companies_e)company)) /
+                                     - evtolLst.getCruiseMinEnergy((evtol_companies_e)company)) /
                                     evtolLst.getCruiseMinEnergy((evtol_companies_e)company)) - 1;
         cout << "vFlightMins = " << vFlightMins << endl;
         v.startFlight(1);
@@ -100,6 +116,20 @@ void unitTest( void )
         assert(v.getCurrentState() == VS_IDLE);
         //v.disp();
     }
+}
+
+/// \brief Bottom up unit test.
+/// TODO: Install google test then utilize mock classes for a top-down testing.
+void unitTest( void )
+{
+    evtol_prop_unitTest();
+    evtol_list_unitTest();
+    charge_stations_unitTest();
+    vehicle_unitTest();
+
+    // Note the veh_sim high-level class was not unit-tested.
+    // The veh_sim class was tested by running the simulation with a log file.
+    // In the future, a mock-class test frame work could be utilized for testing veh_sim class.
 }
 
 int main()
